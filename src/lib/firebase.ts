a691ac340bdcda680de39b4aaab343ea533b7c01
Patryk <patrykr124@gmail.com>
2025-07-30 16:06:48 +0200

    modified:   src/lib/firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBZPjSiiX3cDjhiOevZhFECBQcoUmwAV5s",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_KEY,
   authDomain: "sellaro-cdb9f.firebaseapp.com",
   projectId: "sellaro-cdb9f",
   storageBucket: "sellaro-cdb9f.firebasestorage.app",
